b6999c9a6dd283722607811f329767307d58777d
23-faisal <a.faisal23r@gmail.com>
2026-08-16 07:30:16 +0600

    create teacher and parent page ui

--- app/(dashboard)/parent/page.tsx
+++ app/(dashboard)/parent/page.tsx
@@ -1,7 +1,24 @@
-
+import AnnouncementComponent from "@/components/shared/AnnouncementComponent";
+import BigCalendar from "@/components/shared/BigCalendar";
 
 const ParentPage = () => {
-  return <div>ParentPage</div>;
+  return (
+    <div className="p-4 flex flex-col gap-4 xl:flex-row">
+      {/* left  */}
+      <div className="w-full xl:w-2/3 ">
+        <div className="bg-white p-4 rounded-xl ">
+          <h1 className="text-xl font-semibold ">Schedule (John Doe)</h1>
+          <div className="h-full">
+            <BigCalendar />
+          </div>
+        </div>
+      </div>
+      {/* right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <AnnouncementComponent />
+      </div>
+    </div>
+  );
 };
 
 export default ParentPage;
--- app/(dashboard)/teacher/page.tsx
+++ app/(dashboard)/teacher/page.tsx
@@ -1,5 +1,24 @@
+import AnnouncementComponent from "@/components/shared/AnnouncementComponent";
+import BigCalendar from "@/components/shared/BigCalendar";
+
 const TeacherPage = () => {
-  return <div>TeacherPage</div>;
+  return (
+    <div className="p-4 flex flex-col gap-4 xl:flex-row">
+      {/* left  */}
+      <div className="w-full xl:w-2/3 ">
+        <div className="bg-white p-4 rounded-xl ">
+          <h1 className="text-xl font-semibold ">Schedule</h1>
+          <div className="h-full">
+            <BigCalendar />
+          </div>
+        </div>
+      </div>
+      {/* right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <AnnouncementComponent />
+      </div>
+    </div>
+  );
 };
 
 export default TeacherPage;
